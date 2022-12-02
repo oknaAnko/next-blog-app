b656bf683725c3fdf5d367b991404e94be484e37
annajedrzejko <anna.jedrzejko@elpassion.pl>
2022-12-02 08:38:43 +0100

    fix: Tailwindcss styles don't apply on page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,36 @@
+// module.exports = {
+//   purge: ['./components/**/*.{js,ts,jsx,tsx}', './pages/**/*.{js,ts,jsx,tsx}'],
+//   darkMode: 'media', // 'media' or 'class'
+//   theme: {
+//     extend: {
+//       colors: {
+//         'accent-1': '#333'
+//       }
+//     }
+//   },
+//   variants: {
+//     extend: {}
+//   },
+//   plugins: []
+// };
+
 module.exports = {
-  purge: ['./components/**/*.{js,ts,jsx,tsx}', './pages/**/*.{js,ts,jsx,tsx}'],
-  darkMode: 'media', // 'media' or 'class'
+  content: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {
-      colors: {
-        'accent-1': '#333'
-      }
-    }
-  },
-  variants: {
     extend: {}
   },
   plugins: []
 };
+
+// const { join } = require('path');
+
+// module.exports = {
+//   content: [
+//     join(__dirname, './pages/**/*.{js,ts,jsx,tsx}'),
+//     join(__dirname, './src/**/*.{js,ts,jsx,tsx}')
+//   ],
+//   theme: {
+//     extend: {}
+//   },
+//   plugins: []
+// };
