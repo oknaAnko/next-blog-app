2b7f5983cdd2078fe5625d8451dda63fe6c2aa05
annajedrzejko <anna.jedrzejko@elpassion.pl>
2023-02-26 10:27:46 +0100

    fix(wip): sharedArrayBuffer is not defined

--- next.config.js
+++ next.config.js
@@ -4,7 +4,45 @@ const nextConfig = {
   // https://github.com/vercel/next.js/discussions/42994
   experimental: {
     newNextLinkBehavior: false
+  },
+  async headers() {
+    return [
+      {
+        source: '/',
+        headers: [
+          {
+            key: 'Cross-Origin-Embedder-Policy',
+            value: 'require-corp'
+          },
+          {
+            key: 'Cross-Origin-Opener-Policy',
+            value: 'same-origin'
+          }
+        ]
+      }
+    ];
   }
 };
 
-module.exports = nextConfig;
+// module.exports = nextConfig;
+
+module.exports = (phase, { nextConfig }) => {
+  return {
+    ...nextConfig,
+
+    webpack: (config) => {
+      config.resolve = {
+        ...config.resolve,
+        fallback: {
+          fs: false,
+          net: false,
+          path: false,
+          os: false,
+          child_process: false,
+          tls: false
+        }
+      };
+      return config;
+    }
+  };
+};
