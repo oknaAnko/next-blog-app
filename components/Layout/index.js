697cf2562a902d9b15fb7c8eee6cee810b4dde93
annajedrzejko <anna.jedrzejko@elpassion.pl>
2023-02-26 10:24:32 +0100

    fix: Remove <a> from Link

--- components/Layout/index.js
+++ components/Layout/index.js
@@ -12,8 +12,8 @@ export default function Layout({ children }) {
     <div className="p-10 font-mono max-w-screen-xl mx-auto">
       <nav className="bg-blue-400 p-5 flex justify-center">
         {navLinks.map(link => (
-           <Link href={link.path} key={link.id}>
-           <a className="shadow text-slate-600 md:w-40 bg-blue-50 p-2 m-2 text-center hover:bg-gray-50">{link.label}</a>
+           <Link href={link.path} key={link.id} className="shadow text-slate-600 md:w-40 bg-blue-50 p-2 m-2 text-center hover:bg-gray-50">
+           {link.label}
          </Link>
         ))}
       </nav>
